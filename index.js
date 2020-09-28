2fc1bbda7d666c602e4779dc39c7fb3076b7ae2b
haianh2307 <haianh230797@gmail.com>
2020-09-28 09:00:33 +0700

    add index.js

--- index.js
+++ index.js
@@ -1,28 +1,41 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 const server = require("http").createServer(app);
 const io = require("socket.io").listen(server);
 
-const port  = 3000
+const port = 3000;
 
-let arr = []
-io.on('connection',socket =>{
-    console.log("a user connected :D", socket.id);
-    socket.on('chat message', msg =>{
-        arr.unshift(...msg)
-          console.log('on listen message : ' + msg);
-        io.emit('chat message',arr)
-    })
-})
+let arrChan = [];
+let arrLe = [];
+let connectionCount = 0;
+io.on("connection", (socket) => {
+  connectionCount++;
+  console.log("connection count: ", connectionCount);
+  if (connectionCount % 2 === 0) {
+    socket.room = 'chan';
+    socket.join("chan");
+  } else {
+    socket.join("le");
+    socket.room = 'le';
+  }
+  console.log("a user connected :D", socket.id);
+  socket.on("chat message", (msg) => {
+    console.log(socket.room)
+    if (socket.room === 'chan') {
+      arrChan.unshift(...msg);
+      io.to('chan').emit("chat message", arrChan);
+    } else {
+      arrLe.unshift(...msg);
+      io.to('le').emit("chat message", arrLe);
+    }
+    console.log("on listen message : " + msg);
+  });
+});
 
-
-app.get('/', (req, res) =>{
-    res.send('hello')
-})
+app.get("/", (req, res) => {
+  res.send("hello");
+});
 
 server.listen(port, () => {
-    console.log('server is running')
-})
-
-
-
+  console.log("server is running");
+});
