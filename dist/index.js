b5456ff4d512bead176410d947d580d45f96bffd
Nguyen Hai Anh <haianh230797@gmail.com>
2020-10-03 18:13:11 +0700

    fix UI

--- dist/index.js
+++ dist/index.js
@@ -27,6 +27,16 @@ let arr = [
       avatar: 'https://placeimg.com/140/140/any',
     },
   },
+  {
+    _id: 1441543,
+    image: 'https://firebasestorage.googleapis.com/v0/b/chat-app-67c61.appspot.com/o/photos%2F89572368.4628666%2F1601719932327.jpg?alt=media&token=661b6eb9-2633-477c-b6c5-72b95f48376c',
+    createdAt: new Date(),
+    user: {
+      _id: 1,
+      name: 'React Native',
+      avatar: 'https://placeimg.com/140/140/any',
+    },
+  },
 ];
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -45,6 +55,9 @@ io.on('connection', (socket) => {
   socket.on('chat image', (msg) => {
     arr.push(msg);
     io.to('chat').emit('chat image', msg);
+  });
+  socket.on('typing', (msg) => {
+    io.to('chat').emit('typing', msg);
   });
 });
 
